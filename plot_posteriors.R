a3c660d127a74d1726fa4122ca6c020b244601f7
Ben Brennan <brennben@umich.edu>
2022-04-15 11:06:51 -0400

    new plotting functions for helpful visual output

--- plot_posteriors.R
+++ plot_posteriors.R
@@ -57,6 +57,122 @@ summarize_posteriors <- function(sim){
 } 
 
 
+plot_gammas <- function(ghs_dat){
+  
+  gam_dat <- data.frame(gam_j_mean = rowMeans(ghs_dat$posterior_draws_gam_j),
+                        gam_k_mean = rowMeans(ghs_dat$posterior_draws_gam_k),
+                        j = ghs_dat$real_dat$signal$j, 
+                        k = ghs_dat$real_dat$signal$k,
+                        sig = ghs_dat$real_dat$signal$signal)
+  
+  gam_dat1 <- gam_dat %>% pivot_longer(-c(gam_k_mean, gam_j_mean, sig)) 
+  connections <- gam_dat1 %>% group_by(value) %>% summarise(connects = sum(sig), sig = ifelse(any(sig == 1),TRUE,FALSE))
+  plt <- gam_dat1 %>% select(-sig) %>% distinct(value, .keep_all = T) %>% 
+    arrange(value) %>% mutate(gam_j_mean = ifelse(name == "k", gam_k_mean, gam_j_mean)) %>%
+    select(-name, - gam_k_mean) %>% left_join(connections) %>% 
+    rename(node = value, gam_mean = gam_j_mean) %>% 
+    ggplot(aes(x = node, y = gam_mean, color = as.factor(sig), shape = as.factor(connects))) + 
+    geom_point(size = 4) + theme_bw() + 
+    labs(shape = "Node Degrees", y = "Mean Posterior Node-Specific Shrinkage Parameter", 
+         x = "Node", color = "True Signal") + 
+    theme(text=element_text(size=7.5))
+  
+  return(plt)
+  
+}
+
+plot_lams <- function(ghs_dat){
+  
+  lam_dat <- data.frame(lam_mean = rowMeans(ghs_dat$posterior_draws_lam),
+                        j = ghs_dat$real_dat$signal$j, 
+                        k = ghs_dat$real_dat$signal$k,
+                        sig = ghs_dat$real_dat$signal$signal
+  )
+  
+  plt <- lam_dat %>% ggplot(aes(x = as.factor(k), y = as.factor(j), color = as.factor(sig), fill = as.factor(sig), alpha = lam_mean)) + 
+    geom_tile() + theme_bw() + labs(alpha = "Mean Posterior Edge-Specific Shrinkage",
+                                    fill = "True Signal", 
+                                    x = "Row Node",
+                                    y = "Column Node") + guides(color = FALSE) + 
+    theme(text=element_text(size=7.5))
+  
+  return(plt)
+}
+
+plot_tau <- function(ghs_dat){
+  
+  tau_dat <- data.frame(tau = ghs_dat$posterior_draws_tau, 
+                        id = 1:length(ghs_dat$posterior_draws_tau)) 
+  plt <- tau_dat %>% ggplot(aes(x = tau)) + geom_density() + theme_bw() + labs(x = "Posterior Tau",
+                                                                               y = "Density") + 
+    theme(text=element_text(size=7.5))
+  return(plt)
+}
+
+plot_betas <- function(ghs_dat){
+  beta_dat <- data.frame(selected = ifelse(ghs_dat$posterior_pe < 0.5, F, T),
+                         j = ghs_dat$real_dat$signal$j, 
+                         k = ghs_dat$real_dat$signal$k,
+                         sig = ghs_dat$real_dat$signal$signal)
+  plt <- beta_dat %>% ggplot(aes(x = as.factor(k), y = as.factor(j), color = as.factor(sig), fill = as.factor(sig), alpha = as.factor(selected))) + 
+    geom_tile() + theme_bw() + labs(alpha = "Selection in Model",
+                                    fill = "True Signal", 
+                                    x = "Row Node",
+                                    y = "Column Node") + guides(color = FALSE) + 
+    theme(text=element_text(size=7.5))
+  
+}
+
+plot_ghs <- function(ghs_dat, plot_graph = T){
+  if(plot_graph)
+    plot(ghs_dat$real_dat$full_ntwork)
+  
+  ((plot_lams(ghs_dat) +  plot_betas(ghs_dat)) / plot_gammas(ghs_dat)) + plot_tau(ghs_dat)
+  
+}
+
+plot_comparison_lams_ghs <- function(ghs1, ghs2, names = NULL){
+  if(!identical(ghs1$real_dat$signal, ghs2$real_dat$signal)){
+    stop("Not the same network data/signal")
+  }
+  
+  lam_dat1 <- data.frame(lam_mean = rowMeans(ghs1$posterior_draws_lam),
+                         j = ghs1$real_dat$signal$j, 
+                         k = ghs1$real_dat$signal$k,
+                         sig = ghs1$real_dat$signal$signal
+  )
+  lam_dat2 <- data.frame(lam_mean = rowMeans(ghs2$posterior_draws_lam),
+                         j = ghs2$real_dat$signal$j, 
+                         k = ghs2$real_dat$signal$k,
+                         sig = ghs2$real_dat$signal$signal
+  )
+  
+  if(!is.null(names)){
+    lam_dat_1 <- cbind.data.frame(lam_dat1, type = names[1] )
+    lam_dat_2 <- cbind.data.frame(lam_dat2, type = names[2] )
+  }
+  
+  connects_j <- lam_dat_1 %>% group_by(j) %>% summarise(j_cons = sum(sig))
+  connects_k <- lam_dat_1 %>% group_by(k) %>% summarise(k_cons = sum(sig))
+  
+  lam_dat <- rbind.data.frame(lam_dat_1, lam_dat_2) %>% left_join(connects_j) %>% left_join(connects_k) %>% 
+    mutate(cons = pmax(j_cons, k_cons))
+  
+  plt <- lam_dat %>% ggplot(aes(x = as.factor(k), y = as.factor(j), 
+                                color = as.factor(sig), fill = as.factor(sig),
+                                shape = as.factor(cons),
+                                size = as.numeric(lam_mean))) + 
+    geom_point() + theme_bw() + labs(size = "Mean Posterior Edge-Specific Shrinkage",
+                                     color = "True Signal", 
+                                     shape = "Connections",
+                                     x = "Row Node",
+                                     y = "Column Node") + guides(fill = FALSE) + 
+    theme(text=element_text(size=7.5)) + facet_wrap(~type)
+  
+  return(plt)
+}
+
+
 
 # also want to compare with emily's method, and linear regression, ridge? and LASSO. 
 #look for software of other new methods
