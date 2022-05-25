7f8773516e4a77d18eb18f882f4101f578da11bb
Ben Brennan <brennben@umich.edu>
2022-05-25 11:22:48 -0400

    added group functionality

--- generate_network_data.R
+++ generate_network_data.R
@@ -8,12 +8,17 @@ generate_network_data <- function(p,
                                   x_sigma = 1,
                                   beta_hubs = F,
                                   hub_nodes = NULL,
-                                  hub_degrees = NULL){
+                                  hub_degrees = NULL,
+                                  groups = F,
+                                  num_groups = NULL,
+                                  group_sizes = NULL,
+                                  num_signals = NULL){
   
   #profvis({
   ## Create a random Adjacency Matrix that represents the network 
   q = p*(p-1)/2
-  if(!beta_hubs){
+  node_specific_shrinkage <- T
+  if(!beta_hubs & !groups){
     adjm <- matrix(NA,p,p)
     true_signals <- matrix(sample(0:1,q , replace = T, prob = c(prop_zero,1-prop_zero)))
     adjm[lower.tri(adjm)] <- true_signals
@@ -22,13 +27,13 @@ generate_network_data <- function(p,
     
     ntwrk <- igraph::graph_from_adjacency_matrix(adjm,
                                                  mode= "undirected")
-  } else {
+  } else if (beta_hubs){
     if(is.null(hub_nodes | is.null(hub_degrees))){
-      error("Please specify nodes AND degress for the hubs")
+      stop("Please specify nodes AND degress for the hubs")
     }
     hub_connects <- list()
     if(p - length(hub_nodes) - sum(hub_degrees) < 2){
-      error("Pick a larger p - your hub and connections are greater than the number of nodes ")
+      stop("Pick a larger p - your hub and connections are greater than the number of nodes ")
     }
     num_nodes <- p
     adjm <- matrix(0,nrow=num_nodes, ncol=num_nodes)
@@ -46,6 +51,64 @@ generate_network_data <- function(p,
     
     ntwrk <- igraph::graph_from_adjacency_matrix(adjm,
                                                  mode="undirected")
+  } else {
+    if(num_groups != length(group_sizes)){
+      stop("Need to enter group size for each group")
+    }
+    if(sum(group_sizes) != p){
+      stop("Sum of group sizes not equal to the size of the network")
+    }
+    for(j in 1:length(group_sizes)){
+      if(num_signals[j] > choose(group_sizes[j],2)){
+        stop(paste("Too many signals in group", j))
+      }
+    }
+    
+    group_membership <- NULL
+    
+    for (i in 1:num_groups){
+      group_membership <- c(group_membership, rep(i, group_sizes[i]))
+  }
+    group_membership_dat <- cbind.data.frame(j = 1:p, k = 1:p, group_membership)
+    
+    i <- 2
+    j <- NULL # rows 
+    k <- NULL # columns 
+    
+    while (i < p+1){
+      addonj <- i:p
+      addonk <- rep(i-1,p+1-i) 
+      j <- c(j,addonj)
+      k <- c(k,addonk)
+      i <- i + 1
+    }
+    jk <- data.frame(j = j, k = k)
+    
+    group_membership_idx <- group_membership_dat %>% select(-k) %>% right_join(jk, by = "j") %>% 
+      rename(j_group = group_membership) %>% rename(j_node = j) %>% left_join(group_membership_dat, by = "k") %>% 
+      rename(k_group = group_membership) %>% select(-j) %>% rename(j = j_node)%>% mutate(possible_signal = ifelse(k_group == j_group, T,F)) 
+    
+    signals <- NULL
+    for (i in 1:num_groups){
+      tmp <- group_membership_idx %>% filter(possible_signal & k_group == i)
+      sigs_in_grp <- num_signals[i]
+      non_permuted_signals <- c(rep(1,sigs_in_grp), rep(0, choose(group_sizes[i],2) - sigs_in_grp))
+      permuted_signals <- sample(non_permuted_signals)
+      dat <- data.frame(j = tmp$j, k = tmp$k, signal = permuted_signals)
+      signals <- rbind(dat,signals)
+    }
+    
+    group_membership_final <- signals %>% right_join(group_membership_idx, by = c("j","k")) %>% 
+      mutate(signal = ifelse(is.na(signal), 0, signal)) %>% arrange(k)
+    adjm <- matrix(NA,p,p)
+    true_signals <- group_membership_final$signal
+    adjm[lower.tri(adjm)] <- true_signals
+    adjm[upper.tri(adjm)] <- t(adjm)[upper.tri(adjm)]
+    diag(adjm) <- 0
+    ntwrk <- igraph::graph_from_adjacency_matrix(adjm,
+                                                 mode= "undirected")
+    node_specific_shrinkage <- F
+    
   }
   
   i <- 2
@@ -94,9 +157,19 @@ generate_network_data <- function(p,
   
   y <- xb + rnorm(n,0,sqrt(noise))
   
+  if(groups){
+    beta_with_indices$k_group = group_membership_final$k_group
+    beta_with_indices$j_group = group_membership_final$j_group
+    beta_with_indices$group = ifelse(group_membership_final$k_group == group_membership_final$j_group,
+                                     group_membership_final$j_group, num_groups +1)
+  } else{
+    beta_with_indices$k_group = k
+    beta_with_indices$j_group = j
+  }
   
   rtrn <- list(outcomes = y, signal = beta_with_indices, ntworkslt = observed_networkslt,
-               p = p, n = n, full_ntwork = ntwrk, adjm = adjm, noise = noise)
+               p = p, n = n, full_ntwork = ntwrk, adjm = adjm, noise = noise,
+               shrinking_nodes = node_specific_shrinkage)
   #})
   return(rtrn)
   
